5ab1f36c7008a61fbfbbda2e28f1b9f6787b78ed
Aylwin <aylwinscw@gmail.com>
2020-11-13 00:41:13 +1100

    modify unit testing

--- test_pacman.py
+++ test_pacman.py
@@ -4,23 +4,23 @@ import pacman
 class TestCase1(unittest.TestCase):
 
     # Change the FILE in pacman.py and run individual test
-    # def test_case1(self):
-    #     self.assertEqual(pacman.report(), 'Output: 0,1,NORTH')
+    def test_case1(self):
+        self.assertEqual(pacman.report(), 'Output: 0,1,NORTH')
 
-    def test_case2(self):
-        self.assertEqual(pacman.report(), 'Output: 0,0,WEST')
+    # def test_case2(self):
+    #     self.assertEqual(pacman.report(), 'Output: 0,0,WEST')
 
-    def test_case3(self):
-        self.assertEqual(pacman.report(), 'Output: 3,3,NORTH')
+    # def test_case3(self):
+    #     self.assertEqual(pacman.report(), 'Output: 3,3,NORTH')
 
-    def test_case4(self):
-        self.assertEqual(pacman.report(), 'Output: 0,4,NORTH')
+    # def test_case4(self):
+    #     self.assertEqual(pacman.report(), 'Output: 0,4,NORTH')
 
-    def test_case5(self):
-        self.assertEqual(pacman.report(), 'Output: 4,0,EAST')
+    # def test_case5(self):
+    #     self.assertEqual(pacman.report(), 'Output: 4,0,EAST')
 
-    def test_case6(self):
-        self.assertEqual(pacman.report(), 'Output: 0,0,WEST')
+    # def test_case6(self):
+    #     self.assertEqual(pacman.report(), 'Output: 0,0,WEST')
 
 # TESTING OUTPUT
 # IE aylwin$ python3 -m unittest test_pacman.py 
